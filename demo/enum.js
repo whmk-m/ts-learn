8a749cc9a76ad7ad142c48ec34b48fde2b2639d9
wang.mao <1454435379@qq.com>
2020-03-12 21:20:57 +0800

    字符串枚举、常量枚举的区别

--- demo/enum.js
+++ demo/enum.js
@@ -1,4 +1,5 @@
 "use strict";
+// 使用枚举我们可以定义一些带名字的常量。 使用枚举可以清晰地表达意图或创建一组有区别的用例。 TypeScript支持数字的和基于字符串的枚举。
 {
     let Color;
     (function (Color) {
@@ -9,4 +10,36 @@
     let c = Color.Blue; // 2
     let g = Color[2]; // 'Blue'
     console.log(c, g);
+}
+{
+    let Days;
+    (function (Days) {
+        Days[Days["Sun"] = 7] = "Sun";
+        Days[Days["Mon"] = 1.5] = "Mon";
+        Days[Days["Tue"] = 2.5] = "Tue";
+        Days[Days["Wed"] = 3.5] = "Wed";
+        Days[Days["Thu"] = 4.5] = "Thu";
+        Days[Days["Fri"] = 5.5] = "Fri";
+        Days[Days["Sat"] = 6.5] = "Sat";
+    })(Days || (Days = {}));
+    ;
+    console.log(Days["Sun"] === 7); // true
+    console.log(Days["Mon"] === 1.5); // true
+    console.log(Days["Tue"] === 2.5); // true
+    console.log(Days["Sat"] === 6.5); // true
+}
+{
+    ;
+    let directions = [0 /* Up */, 1 /* Down */, 2 /* Left */, 10 /* Right */];
+}
+{
+    // 字符串枚举
+    let Direction;
+    (function (Direction) {
+        Direction["Up"] = "UP1";
+        Direction["Down"] = "DOWN1";
+        Direction["Left"] = "LEFT1";
+        Direction["Right"] = "RIGHT1";
+    })(Direction || (Direction = {}));
+    console.log(Direction);
 }
--- demo/enum.ts
+++ demo/enum.ts
@@ -1,6 +1,41 @@
+// 使用枚举我们可以定义一些带名字的常量。 使用枚举可以清晰地表达意图或创建一组有区别的用例。 TypeScript支持数字的和基于字符串的枚举。
 {
     enum Color {Red, Green, Blue}
+
     let c: number = Color.Blue; // 2
     let g: string = Color[2]  // 'Blue'
-    console.log(c,g)
+    console.log(c, g)
+}
+
+{
+    enum Days {Sun = 7, Mon = 1.5, Tue, Wed, Thu, Fri, Sat};
+
+    console.log(Days["Sun"] === 7); // true
+    console.log(Days["Mon"] === 1.5); // true
+    console.log(Days["Tue"] === 2.5); // true
+    console.log(Days["Sat"] === 6.5); // true
+}
+
+
+{
+    // 常数枚举
+    const enum Directions {
+        Up,
+        Down,
+        Left,
+        Right = 10  // 赋值必须是一个常量，不能通过计算而的值
+    };
+    let directions = [Directions.Up, Directions.Down, Directions.Left, Directions.Right];
+}
+
+
+{
+    // 字符串枚举
+    enum Direction {
+        Up = "UP1",
+        Down = "DOWN1",
+        Left = "LEFT1",
+        Right = "RIGHT1",
+    }
+    console.log(Direction)
 }
